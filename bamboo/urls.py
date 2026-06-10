a3c129473a4c7870d39c09e2e5340009505303bc
hangaeul <hangaeul.dev@gmail.com>
2026-06-10 14:37:18 +0900

    [EDIT]

--- bamboo/urls.py
+++ bamboo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/', include('forest.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
